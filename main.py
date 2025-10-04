762a3ff9dad85d0156d1e3068b0ee3b074312e6d
MNone <pokerj204@gmail.com>
2025-10-04 22:37:40 +0330

    send message to  chat

--- main.py
+++ main.py
@@ -5,14 +5,20 @@ from flask import Flask
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 
-# ---------- متغیرهای محیطی ----------
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 SESSION_STRING = os.environ.get("SESSION")
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID"))
-
-# ---------- Telethon client ----------
+BOT_TOKEN=os.environ.get("BOT_TOKEN")
 client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
+
+
+bot=TelegramClient('bot',API_ID,API_HASH).start(bot_token=BOT_TOKEN)
+
+
+@client.on(events.NewMessage)
+async def hanlder(event):
+    text=event.new
 
 
 @client.on(events.NewMessage(chats=[CHANNEL_ID]))
@@ -20,8 +26,8 @@ async def handler(event):
     text = event.message.message or ""
     await client.send_message("me", f"📢 پست جدید:\n\n{text}")
     print("پیام به Saved Messages فرستاده شد ✅")
+    print(text)
 
-# ---------- Flask app برای healthcheck ----------
 app = Flask(__name__)
 
 @app.route("/")
@@ -32,14 +38,11 @@ def run_flask():
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, threaded=True)
 
-# ---------- اجرای همزمان Flask و Telethon ----------
 async def start_telethon():
     await client.start()
     print("Telethon started")
     await client.run_until_disconnected()
 
 if __name__ == "__main__":
-    # Flask در یک thread جداگانه ران شود
     Thread(target=run_flask, daemon=True).start()
-    # Telethon در main thread asyncio اجرا شود
     asyncio.get_event_loop().run_until_complete(start_telethon())
